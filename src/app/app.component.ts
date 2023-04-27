fe7dda1c186e3ed28ab8927a6695e59df2d38695
Lucas <lucas@woowtech.com.br>
2023-04-27 09:09:22 -0300

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'social-media-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'social-media';
+}
